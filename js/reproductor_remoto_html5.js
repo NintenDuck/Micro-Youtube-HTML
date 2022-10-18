d68f415918b6313a4200c87993aac2893eb263e7
NintenDuck <andrecked507@gmail.com>
2022-10-18 17:22:29 -0500

    Add: Basic search functionality when button click

--- js/reproductor_remoto_html5.js
+++ js/reproductor_remoto_html5.js
@@ -1,3 +1,6 @@
+var apiKey = "AIzaSyCveHlrpaGLt35eFLOrX2WeemPJS1GZDv4"
+var personalID = "VP6isH0l3nLU-NIJs875nw"
+
 function load() {
   onYouTubeIframeAPIReady();
 }
@@ -98,6 +101,31 @@ $("#prev").on("click", function () {
 });
 
 // OWN CODE
+
+
+async function foo() {
+  console.log("love lain")
+  // searchBox = document.getElementById("search-bar")
+  // searchQuery = searchBox.value
+  const respuesta = await axios.get("https://www.googleapis.com/youtube/v3/search?",
+  {
+      params: {
+          key: apiKey,
+          q: "minecraft",
+          part: "snippet",
+          order: "relevance",
+          maxResults: 5,
+          type: "music"
+      }
+  })
+  if (respuesta.status === 200){
+      const busqueda = await respuesta.data.items
+      console.log(busqueda)
+      return busqueda
+  } else {
+      console.log("[ERROR] Ocurrio algo en la solicitud")
+  }
+}
 
 
 function addSongToList(songName, authorName, userName) {
