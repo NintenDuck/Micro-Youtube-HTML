c0004d555b004460ec59f812b7f1353a1f4ac830
NintenDuck <andrecked507@gmail.com>
2022-10-28 11:10:05 -0500

    Add: Project Re-structure

--- js/player_controller.js
+++ js/player_controller.js
@@ -58,31 +58,3 @@ function changePlayButtonState() {
 
   return currentBtnState
 }
-
-// **********************************************************
-// CODIGO IFRAME API
-// **********************************************************
-
-function onload(){
-    onYouTubeIframeAPIReady()
-}
-
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '400px',
-    width: '250',
-    videoId: 'knp2WGkIpLw',
-    playerVars: {
-        playlist: 'knp2WGkIpLw,qelGSWu4s_U'
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChanged
-    }
-  });
-}
-
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
