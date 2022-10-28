e788345cf82e889b5014b1134cdb7de15fda29aa
NintenDuck <andrecked507@gmail.com>
2022-10-28 10:01:41 -0500

    Add: playBtn change on click

--- js/player_controller.js
+++ js/player_controller.js
@@ -4,6 +4,7 @@
 
 function playVideo() {
     player.playVideo()
+    changePlayButtonState()
 }
 
 function nextVideo() {
@@ -17,6 +18,26 @@ function previousVideo() {
 function stopVideo() {
     player.stopVideo();
   }
+
+  // btnStates[0] = Reproduciendo
+  // btnStates[1] = Pausado
+let btnStates = ["Play", "Paused"]
+let currentBtnState = "Paused"
+
+function changePlayButtonState() {
+  const playBtnElement = document.getElementById("controls-play")
+
+  if (currentBtnState == btnStates[1]){
+    playBtnElement.src =  "./resources/svg/pause.png"
+    playBtnElement.style.height = "3rem"
+    currentBtnState = btnStates[0]
+  } else if (currentBtnState == btnStates[0]) {
+    playBtnElement.src =  "./resources/svg/play-outline.svg"
+    playBtnElement.style.height = "4rem"
+    currentBtnState = btnStates[1]
+  }
+  console.log("Current butto state: " + currentBtnState)
+}
 
 // **********************************************************
 // CODIGO IFRAME API
