de32697a37fc5b33d7f199b327e2744fd1b59026
NintenDuck <andrecked507@gmail.com>
2022-11-04 11:43:41 -0600

    Add: Simple websocket client/server communication

--- js/PlayerJS/websocketClient.js
+++ js/PlayerJS/websocketClient.js
@@ -1,1 +1,21 @@
-const Websocket = require("ws")
+const ws = new WebSocket("ws://localhost:8008")
+
+let username = "Lain"
+let msgFormat = {
+    userName: username,
+    message: ""
+}
+
+// let stringifiedMsg = JSON.stringify(msgFormat)
+// console.log(stringifiedMsg)
+// console.log(typeof(stringifiedMsg))
+
+ws.addEventListener("open", () => {
+    console.log("We are connected")
+    
+    ws.send(JSON.stringify(msgFormat))
+})
+
+ws.addEventListener("message", ({ dataReceived }) => {
+  console.log(JSON.stringify(dataReceived))
+})
--- js/Websocket/websocketServer.js
+++ js/Websocket/websocketServer.js
@@ -1,17 +1,38 @@
 const WebSocket = require("ws")
 
-const wss = new WebSocket.Server({port: 8008})
+const wss = new WebSocket.Server({
+    port: 8008
+})
+
+usersConnected = 0
 
 wss.on("connection", (ws) => {
-    console.log("New client connected!")
+    countUsersConnected(ws)
 
-    ws.on("message", data => {
-        console.log(`Client just sent this message: ${data}`)
+    ws.on("message", data => {onMessage(data, ws)})
 
-        ws.send(data.toString())
-    })
-    
-    ws.on("close", () => {
-        console.log("Client has disconnected")
-    })
+    ws.on("close", () => {onCloseConnection()})
 })
+
+
+function countUsersConnected(websocket) {
+    usersConnected += 1
+    serverMessage("New client just connected")
+    serverMessage("Clients connected: " + usersConnected)
+}
+function onMessage(messageData, websocket) {
+    serverMessage("Client just sent this message:")
+    console.log(messageData)
+
+    websocket.send("[SERVER] You have connected succesfully to the server!")
+}
+function onCloseConnection() {
+    console.log("Client has disconnected")
+    usersConnected -= 1
+    serverMessage(`[SERVER] Clients connected: ${usersConnected}`)
+}
+
+
+function serverMessage(message) {
+    console.log(`[SERVER] ${message}`)
+}
